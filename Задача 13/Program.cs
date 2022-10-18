ee43811fbd012061b3d23d4e5d700c1bdb8bf7e1
Ekaterina Voitkovich <styardess@mail.ru>
2022-10-18 20:48:27 +0300

    Задача 10

---
+++ Задача 13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+
+while (A < 1000)
+{
+   if (A / 10);
+   Console.ReadLine(B);
+
+   if int C = ( B / 10);
+    Console.WriteLine($"Третье число равно {C}");
+}
