a184a9614033e8094530acd50e5b4ed2c9af7fd5
Ekaterina Voitkovich <styardess@mail.ru>
2022-10-18 22:41:59 +0300

    Надо закончить

--- Задача 13/Program.cs
+++ Задача 13/Program.cs
@@ -1,11 +1,29 @@
-﻿Console.WriteLine("Введите число");
-int A = Convert.ToInt32(Console.ReadLine());
+﻿//Программа, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
 
-while (A < 1000)
+
+
+internal class Program
 {
-   if (A / 10);
-   Console.ReadLine(B);
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int A = Convert.ToInt32(Console.ReadLine());
 
-   if int C = ( B / 10);
-    Console.WriteLine($"Третье число равно {C}");
+        while (A > 99)
+
+        {
+            int B = (A / 10);
+            int C = (B % 10);
+        
+            A = A / 10;
+            
+            Console.WriteLine($"Третья цифра в числе - {C}");
+        }
+    
+        if (A < 100)
+        {
+            Console.WriteLine("Третья цифра отсутствует");
+        }
+    }
 }
