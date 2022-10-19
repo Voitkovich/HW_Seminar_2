bb0c441bb35940a0361c3a1aa4b168ac40532ac1
Ekaterina Voitkovich <styardess@mail.ru>
2022-10-19 21:17:04 +0300

    13 задача решена

---
+++ new/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastnumber = number % 10;
+
+if (number > 99) 
+{
+    while (number > 1000)
+{
+    number = (number / 10);
+}
+    Console.WriteLine($"Третья цифра заданного числа: {lastnumber}");
+}
+else 
+{
+    Console.WriteLine("Нет трёх чисел");
+}
--- Задача 13/Program.cs
+++ Задача 13/Program.cs
@@ -2,31 +2,25 @@
 // или сообщает, что третьей цифры нет.
 
 
-
-internal class Program
-{
-    private static void Main(string[] args)
-    {
         Console.WriteLine("Введите число");
         int A = Convert.ToInt32(Console.ReadLine());
-        int find = A % 10;
+        
 
         if (A > 99)
-
-        {
+{
+        
             while  ( A > 1000)
         {
             A = (A / 10);
         }
-        
-      Console.WriteLine($"Третья цифра в числе - {find}");
-        }
+     
+      Console.WriteLine($"Третья цифра в числе - {A % 10}");
+} 
         else
-    {
+    
         {
             Console.WriteLine("Третья цифра отсутствует");
         }
-        }
-}    
-}
+        
+
     
